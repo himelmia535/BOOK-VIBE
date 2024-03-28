eedf125d21fb81c6939becc7499592119daa1282
himelmia535 <himelmia535@gmail.com>
2024-03-28 18:57:45 +0600

    books blogs show

---
+++ src/components/Blog.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+const Blog = ({blog}) => {
+    const{bookId, bookName, author, image, review, totalPages, rating, category, tags, publisher, yearOfPublishing} = blog;
+
+    return (
+        
+        <div className="border border-primary rounded-2xl p-4 m-2 ">
+
+            <Link to={`/blog/${blog.bookId}`} className="max-w-sm mx-auto dark:bg-gray-50">
+                <img role="presentation" className="object-cover w-full rounded h-screen  dark:bg-gray-500" src={image} />
+                <div className="flex gap-2 text-green-500 font-bold m-2">
+                    <h3 className="bg-slate-200 p-2 rounded-3xl">{tags[0]}</h3>
+                    <h3 className="bg-slate-200 p-2 rounded-3xl">{tags[1]}</h3>
+                    <h3 className="bg-slate-200 p-2 rounded-3xl">{tags[2]}</h3>
+                </div>
+                <div className="p-6 space-y-2">
+                    <h3 className="text-3xl font-bold">{bookName}</h3>
+                    <h3 className="text-lg font-semibold">By: {author}</h3>
+
+                    <h5 className="border-b-2 m-2 border-dashed mx-auto"></h5>
+
+                    <div className="flex justify-between text-lg">
+                        <span>{category}</span>
+                        <span>{rating}</span>
+                    </div>
+                </div>
+            </Link>
+                
+        </div>
+    );
+};
+
+export default Blog;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import Banner from "../components/Banner";
+import Blogs from "../components/Blogs";
 
 const Home = () => {
     return (
         <div className="m-8">
             <Banner></Banner>
+            <div className="text-center text-4xl font-bold mt-14">Books</div>
+            <Blogs></Blogs>
         </div>
     );
 };
